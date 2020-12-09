a101f04b5e683ba39d945844ee01a498c18a29fb
atokarev77 <atokarev77@mail.ru>
2020-12-09 21:41:10 +0900

    Добавлены тесты на каждый метод калькулятора

--- tests/calc_test.py
+++ tests/calc_test.py
@@ -1,4 +1,5 @@
 from app.calculator import Calculator
+import pytest
 
 
 class TestCalc:
@@ -7,7 +8,15 @@ class TestCalc:
         self.calc = Calculator()
 
     def test_multiply_correct(self):
-        assert self.calc.multiply(2, 2) == 4
+        assert self.calc.multiply(10, 2) == 20
 
-    def test_multiply_correct(self):
-        assert self.calc.multiply(2, 2) == 4
+    def test_division_correct(self):
+        assert self.calc.division(10, 2) == 5.0
+        with pytest.raises(ZeroDivisionError) as zero_division:
+            self.calc.division(10, 0)
+
+    def test_subtraction_correct(self):
+        assert self.calc.subtraction(100, 18) == 82
+
+    def test_adding_correct(self):
+        assert self.calc.adding(30, 11) == 41
